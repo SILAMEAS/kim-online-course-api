93d95f2a002d8596aebc1280e03c8383e400ae19
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2026-03-14 12:42:07 +0700

    push

--- src/main/java/com/sila/config/jpaConfig/SpringSecurityAuditorAware.java
+++ src/main/java/com/sila/config/jpaConfig/SpringSecurityAuditorAware.java
@@ -1,20 +1,20 @@
-package com.sila.config.jpaConfig;
-
-import com.sila.config.context.UserContext;
-import java.util.Optional;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SpringSecurityAuditorAware implements AuditorAware<Long> {
-
-  @Override
-  public Optional<Long> getCurrentAuditor() {
-
-    try {
-      return Optional.of(UserContext.getUserId());
-    } catch (NumberFormatException e) {
-      return Optional.empty(); // fallback if conversion fails
-    }
-  }
-}
+//package com.sila.config.jpaConfig;
+//
+//import com.sila.config.context.UserContext;
+//import java.util.Optional;
+//import org.springframework.data.domain.AuditorAware;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class SpringSecurityAuditorAware implements AuditorAware<Long> {
+//
+//  @Override
+//  public Optional<Long> getCurrentAuditor() {
+//
+//    try {
+//      return Optional.of(UserContext.getUserId());
+//    } catch (NumberFormatException e) {
+//      return Optional.empty(); // fallback if conversion fails
+//    }
+//  }
+//}
