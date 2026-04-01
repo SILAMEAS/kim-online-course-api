dbeabdcce51d11e6cc4b13f37c74f8ade9a21a83
Meas Sila <Las239879@gmail.com>
2026-04-01 20:09:37 +0700

    response course in list videos

--- src/main/java/com/sila/modules/video/dto/VideoListResponse.java
+++ src/main/java/com/sila/modules/video/dto/VideoListResponse.java
@@ -1,5 +1,6 @@
 package com.sila.modules.video.dto;
 
+import com.sila.modules.course.dto.CourseResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,6 @@ public class VideoListResponse {
   private String publicId;
 
   private Integer duration;
+
+  private CourseResponse course;
 }
